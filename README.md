3216c9547c2b484598de2e6f326323f83b444408
seansan <sheukels+github@gmail.com>
2015-11-17 20:54:22 +0100

    Initial upload

---
+++ README.md
@@ -0,0 +1,7 @@
+SNH_CategoryNoindex
+===================
+
+Magento extension that adds NOINDEX to filtered category pages
+
+Disable by removing modman directory
+
---
+++ SNH/CategoryNoindex/Model/Observer.php
@@ -0,0 +1,18 @@
+<?php
+class SNH_CategoryNoindex_Model_Observer
+{
+
+	public function changeRobots($observer) {
+	if($observer->getEvent()->getAction()->getFullActionName() == 'catalog_category_view') {
+		$uri = $observer->getEvent()->getAction()->getRequest()->getRequestUri();
+		if(stristr($uri,"?")): // looking for a ?
+			$layout       = $observer->getEvent()->getLayout();
+			$product_info = $layout->getBlock('head');
+			$layout->getUpdate()->addUpdate('<reference name="head"><action method="setRobots"><value>NOINDEX,FOLLOW</value></action></reference>');
+			$layout->generateXml();
+		endif;
+	}
+	return $this;
+	}
+
+}
